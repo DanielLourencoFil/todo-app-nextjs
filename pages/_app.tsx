3ed6dfe39ac5b03a6aa8b934cf1c823a33580e22
daniellourencoedu <daniellourencoedu@gmail.com>
2022-10-05 16:46:34 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { darkTheme, lightTheme } from "../Themes";
+import "../styles/globals.css";
+import { UIProvider } from "../context/ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<UIProvider>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</UIProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
